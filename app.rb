82b7091c3316eb231669884ba44fc12ce1bf2fd8
iMprov <improv00@mail.ru>
2025-07-02 19:53:51 +0700

    передаем парикмахеров на пост обработчик

--- app.rb
+++ app.rb
@@ -28,8 +28,9 @@ post '/visit' do
 	@username = params[:username].to_s
 	@number = params[:number].to_s
 	@datetime = params[:datetime].to_s
+	@barber = params[:barber].to_s
 
-	input_user = {:username => "Введите имя!", :number => "Укажите ваш телефон для связи!",:datetime => "Вы не указали дату когда придете!"}
+	input_user = {:username => "Введите имя!", :number => "Укажите ваш телефон для связи!",:datetime => "Вы не указали дату когда придете!", :barber => "Вы не выбрали парикмахера!"}
 
 	input_user.each do |key, values|
 		if params[key].to_s.strip.empty?
